ae14198e537e960c172901f7d66ae79524c6c4b4
underlapro <underlapro@gmail.com>
2024-12-06 22:14:03 -0300

    Update README.mk
    entrega trabalho

--- README.mk
+++ README.mk
@@ -1,1 +1,11 @@
 AI900LABAULA1
+
+Trabalhando com Machine Learning na Prática no Azure ML
+
+Criando meu modelo preditivo: Acesse o link a seguir >>> [https://microsoftlearning.github.io/mslearn-ai-fundamentals/Instructions/Labs/01-machine-learning.html]
+
+Dados usaddos no teste:
+
+{ "Inputs": { "data": [ { "Path": "example_value", "day": 5, "mnth": 4, "year": 2023, "season": 1, "holiday": 5, "weekday": 3, "workingday": 5, "weathersit": 2, "temp": 0.4, "atemp": 0.4, "hum": 0.1, "windspeed": 0.6 } ] }, "GlobalParameters": 0.8 }
+
+Resultado do teste: "Results": [ 5255.6542163497833526 ]
